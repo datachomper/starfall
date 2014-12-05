7153e02a4b331bd9e61cc633347673dce11e80b4
Russ Meyerriecks <datachomper@gmail.com>
2014-12-04 23:09:43 -0600

    SDL: Bugfix for v2.0.3 "winapifamily.h" no such file on compile
    http://stackoverflow.com/questions/22446008/winapifamily-h-no-such-file-or-directory-when-compiling-sdl-in-codeblocks

--- SDL2-2.0.3/include/SDL_platform.h
+++ SDL2-2.0.3/include/SDL_platform.h
@@ -118,7 +118,7 @@
 /* Try to find out if we're compiling for WinRT or non-WinRT */
 /* If _USING_V110_SDK71_ is defined it means we are using the v110_xp or v120_xp toolset. */
 #if defined(__MINGW32__) || (defined(_MSC_VER) && (_MSC_VER >= 1700) && !_USING_V110_SDK71_)	/* _MSC_VER==1700 for MSVC 2012 */
-#include <winapifamily.h>
+//#include <winapifamily.h>
 #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
 #undef __WINDOWS__
 #define __WINDOWS__   1
@@ -129,7 +129,7 @@
 #endif
 #else
 #undef __WINDOWS__
-#define __WINDOWS__   1
+//#define __WINDOWS__   1
 #endif /* _MSC_VER < 1700 */
 #endif /* defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__) */
 
