59dd2218ffabe3620507238cc8e9842c3812cd56
Russ Meyerriecks <datachomper@gmail.com>
2014-12-05 21:15:42 -0600

    Just bliting a bitmap to the screen

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 @echo off
-set INCLUDE=%cd%\SDL2-2.0.3\include
-%INCLUDE%
 mkdir build
 pushd build
-cl -Zi ../game.cpp sdl.lib user32.lib gdi32.lib
+cl -Zi /IC:\Users\Russ\src\boilerplate\SDL2-2.0.3\include ../game.cpp C:\Users\Russ\src\boilerplate\SDL2-2.0.3\lib\x64\SDL2.lib user32.lib gdi32.lib
 popd
--- game.cpp
+++ game.cpp
@@ -4,11 +4,37 @@
 
 int main(int argc, char **argv)
 {
-	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER)) {
+	SDL_Window *window;
+	SDL_Surface *surface;
+	SDL_Surface *image;
+
+	if (SDL_Init(SDL_INIT_VIDEO)) {
 		fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
 		return 1;
 	}
-	atexit(SDL_Quit);
 
+	window = SDL_CreateWindow("boilerplate",
+				  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+				  800, 600,
+				  0);
+	if (window == NULL) {
+		printf("Oops, couldn't create window brah\n");
+		return -1;
+	}
+
+	surface = SDL_GetWindowSurface(window);
+	image = SDL_LoadBMP("unicorn.bmp");
+	if (image == NULL) {
+		printf("Unable to load unicorn: %s\n", SDL_GetError());
+	}
+
+	SDL_BlitScaled(image, NULL, surface, NULL);
+	SDL_FreeSurface(image);
+
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
