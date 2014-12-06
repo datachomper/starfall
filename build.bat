69609ef7665e5d784875cac61896410cda650c86
Russ Meyerriecks <datachomper@gmail.com>
2014-12-06 13:45:24 -0600

    Clicking to destroy actors works

--- build.bat
+++ build.bat
@@ -3,3 +3,4 @@ mkdir build
 pushd build
 cl -Zi /IC:\Users\Russ\src\boilerplate\SDL2-2.0.3\include ../game.cpp C:\Users\Russ\src\boilerplate\SDL2-2.0.3\lib\x64\SDL2.lib user32.lib gdi32.lib
 popd
+build\game.exe
--- game.cpp
+++ game.cpp
@@ -1,6 +1,23 @@
+/* Copyright 2014 Russ Meyerriecks <datachomper@gmail.com> */
+/* Quick game for Ludum Dare #31 */
+/* Audio generated using BFXR http://www.bfxr.net/ */
+
 #include "SDL.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define FRAMERATE 60
+#define MS_PER_FRAME ((1.0/FRAMERATE)*1000)
+//#define MS_PER_FRAME 17
+#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 300
+#define MAX_ACTORS 1000
+
+struct Actor {
+	bool enabled;
+	SDL_Rect rect;
+};
 
 int main(int argc, char **argv)
 {
@@ -15,7 +32,7 @@ int main(int argc, char **argv)
 
 	window = SDL_CreateWindow("boilerplate",
 				  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				  800, 600,
+				  WINDOW_WIDTH, WINDOW_HEIGHT,
 				  0);
 	if (window == NULL) {
 		printf("Oops, couldn't create window brah\n");
@@ -23,17 +40,83 @@ int main(int argc, char **argv)
 	}
 
 	surface = SDL_GetWindowSurface(window);
-	image = SDL_LoadBMP("unicorn.bmp");
-	if (image == NULL) {
-		printf("Unable to load unicorn: %s\n", SDL_GetError());
+
+	/* Set background to black */
+	SDL_FillRect(surface, &surface->clip_rect, 0);
+
+	/* TODO temp make a star and move it */
+	int velocity = 1; // Star falling speed in pixels/second
+	Actor *actor = (Actor *)malloc(sizeof(actor[0]) * MAX_ACTORS);
+	memset(actor, 0, sizeof(actor[0]) * MAX_ACTORS);
+
+	bool running = true;
+	while (running) {
+		unsigned int start = SDL_GetTicks();
+
+		/* Process events */
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT)
+				running = false;
+			if (e.type == SDL_MOUSEBUTTONDOWN) {
+				//printf("mouse x: %d y: %d\n", e.button.x, e.button.y);
+
+				/* Detect clicking on an actor */
+				for (int i = 0; i < MAX_ACTORS; i++) {
+					if (actor[i].enabled) {
+						if ((e.button.x >= actor[i].rect.x) &&
+						    (e.button.x <= (actor[i].rect.x + actor[i].rect.w)) &&
+						    (e.button.y >= actor[i].rect.y) &&
+						    (e.button.y <= (actor[i].rect.y + actor[i].rect.h))) {
+
+							actor[i].enabled = false;
+						    }
+					}
+				}
+			}
+		}
+
+		/* Update */
+		/* Should we spawn actor this frame? */
+		if ((rand() % 60) == 30) {
+			/* Find the first free actor in the array */
+			for (int i = 0; i < MAX_ACTORS; i++) {
+				if (!actor[i].enabled) {
+					/* Randomize spawn location */
+					int x = (rand() % (WINDOW_WIDTH-10));
+					actor[i].rect = {x, 0, 10, 10};
+					actor[i].enabled = true;
+					break;
+				}
+			}
+		}
+
+		/* Blank screen in prep for redraw */
+		/* TODO Maybe dim the old background to make a smear effect */
+		SDL_FillRect(surface, &surface->clip_rect, 0);
+
+		for (int i = 0; i < MAX_ACTORS; i++) {
+			if (actor[i].enabled) {
+				if (actor[i].rect.y > WINDOW_HEIGHT) {
+					actor[i].enabled = false;
+				} else {
+					actor[i].rect.y += velocity;
+					SDL_FillRect(surface, &actor[i].rect, -1);
+				}
+			}
+		}
+
+		/* Render */
+		SDL_UpdateWindowSurface(window);
+
+		/* Sleep for any remaining time */
+		int sleeptime = start + MS_PER_FRAME - SDL_GetTicks();
+		if (sleeptime > 0) {
+			//printf("Sleeping for %dms\n", sleeptime);
+			SDL_Delay(sleeptime);
+		}
 	}
 
-	SDL_BlitScaled(image, NULL, surface, NULL);
-	SDL_FreeSurface(image);
-
-	SDL_UpdateWindowSurface(window);
-
-	SDL_Delay(3000);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
