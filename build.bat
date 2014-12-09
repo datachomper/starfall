804158f65efa0cf2828a9c58b0d47777a7a65e39
Russ Meyerriecks <datachomper@gmail.com>
2014-12-09 12:45:42 -0600

    Prevent the extra console window from opening on program load
    Using linker options, I classified the program as "windows" instead of
    console, then adjusted the entry point to still work with main()
    
    ref:
    http://msdn.microsoft.com/en-us/library/f9t8842e.aspx

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-cl -Iinclude game.cpp SDL2.lib SDL2_ttf.lib SDL2_mixer.lib user32.lib gdi32.lib
+cl -Iinclude game.cpp SDL2.lib SDL2_ttf.lib SDL2_mixer.lib user32.lib gdi32.lib -link -subsystem:windows -entry:mainCRTStartup
 game.exe
